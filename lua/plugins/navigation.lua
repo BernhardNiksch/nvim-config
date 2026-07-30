a229376d991f4e61a877ee933fa7429e7b869859
Bernhard Niksch <bernhard.n@futureforex.co.za>
2026-07-30 20:41:06 +0200

    Fix telescope treesitter errors by setting branch to main

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        branch = "main",
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-ui-select.nvim",
@@ -41,9 +41,7 @@ return {
         config = function()
             require("telescope").setup({
                 extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
+                    ["ui-select"] = require("telescope.themes").get_dropdown({}),
                 },
             })
             require("telescope").load_extension("ui-select")
